d9c1a1176e841b14cd4023dd63fe684b0617d12f
arash <arash.asnaashari@gmail.com>
2020-09-21 22:09:01 +0330

    .....

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-
 module.exports = {
-    mongoURI: 'mongodb+srv://admin:admin@cluster0.dcr08.mongodb.net/test?retryWrites=true'
+  mongoURI:
+    "mongodb+srv://admin:admin@cluster0.dcr08.mongodb.net/test?retryWrites=true",
 };
